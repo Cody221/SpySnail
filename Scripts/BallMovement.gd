41372f05b6b652310bb25fc40cc88b09eeb931fd
DESKTOP-NPF4KP6\AIMM_Default <clrhodes21@my.trine.edu>
2024-08-02 12:54:07 -0400

    added debug drawing and mostly working as intended movement for marble

--- Scripts/BallMovement.gd
+++ Scripts/BallMovement.gd
@@ -1,28 +1,29 @@
 extends RigidBody3D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var direction
-var start = 0
-var end
+#var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var direction = Vector3.ZERO
+var start = Vector3.ZERO
+var end = Vector3.ZERO
 
 @onready var cam = $YGimbal/XGimbal/Camera3D
 
 func _physics_process(_delta):
-	
 	if(Input.is_action_just_pressed("LeftMouse")):
 		start = get_viewport().get_mouse_position()
 		start = cam.project_position(start, cam.position.z)
+		
 	
-	if(Input.is_action_just_released("LeftMouse")):
+	if(Input.is_action_pressed("LeftMouse")):
 		end = get_viewport().get_mouse_position()
 		end = cam.project_position(end, cam.position.z)
+		direction = start - end
+	
+	if(Input.is_action_just_released("LeftMouse")):
+		linear_velocity = direction
+		direction = Vector3.ZERO
 		
-		direction = (end - start)
-		
-		apply_impulse(Vector3(-direction.x, direction.y, direction.z))
-		
-		
+
 
 
 
--- Scripts/Debug/DebugDraw3D.gd
+++ Scripts/Debug/DebugDraw3D.gd
@@ -11,11 +11,15 @@ class Vector:
 	var color
 
 
+func _process(_delta):
+	queue_redraw()
+
 func _draw():
 	var color = Color(0, 1, 0)
 	var start = cam.unproject_position(ball.global_transform.origin)
-	var end = cam.unproject_position(ball.global_transform.origin + ball.linear_velocity)
+	var end = cam.unproject_position(ball.global_transform.origin + ball.direction)
 	draw_line(start, end, color, 1)
+	DrawTriangle(end, end - start, 0.1, color)
 
 func DrawTriangle(pos, dir, size, color):
 	var a = pos + dir * size
