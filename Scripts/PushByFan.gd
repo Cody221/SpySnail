07864e9974f431e7620beb1ec991a2723f7e62a3
Cody <rhodescody20@gmail.com>
2024-09-04 16:45:15 -0400

    Work being done on fan

--- Scripts/PushByFan.gd
+++ Scripts/PushByFan.gd
@@ -7,13 +7,21 @@ func _physics_process(_delta):
 	var space = get_world_3d().direct_space_state
 	#start of ray is position and range is the fan range
 	#seperate query for each point around the fan plus the center 
-	var query0 = PhysicsRayQueryParameters3D.create(position, position + Vector3(0, fanRange, 0))
-	var query1 = PhysicsRayQueryParameters3D.create(position + Vector3(0, 0, 0.5), position + Vector3(0, fanRange, 0))
-	var query2 = PhysicsRayQueryParameters3D.create(position + Vector3(0, 0, -0.5), position + Vector3(0, fanRange, 0))
-	var query3 = PhysicsRayQueryParameters3D.create(position + Vector3(0.5, 0, 0), position + Vector3(0, fanRange, 0))
-	var query4 = PhysicsRayQueryParameters3D.create(position + Vector3(-0.5, 0, 0), position + Vector3(0, fanRange, 0))
 	
-	var array = [query0, query1, query2, query3, query4]
+	#need to factor the rotation of x and z axis to correctly push ball
+	var fanDirection = Vector3(rotation.x, 0, rotation.z).normalized()
+
+	
+	var r = Vector3(fanRange, fanRange, fanRange).bounce(rotation)
+	global_rotation
+	
+	var query0 = PhysicsRayQueryParameters3D.create(position, position + rotation.normalized() * fanRange)
+	#var query1 = PhysicsRayQueryParameters3D.create(position + Vector3(0, 0, 0.5), position + rangeVector)
+	#var query2 = PhysicsRayQueryParameters3D.create(position + Vector3(0, 0, -0.5), position + rangeVector)
+	#var query3 = PhysicsRayQueryParameters3D.create(position + Vector3(0.5, 0, 0), position + rangeVector)
+	#var query4 = PhysicsRayQueryParameters3D.create(position + Vector3(-0.5, 0, 0), position + rangeVector)
+	
+	var array = [query0]
 	
 	var result
 	
@@ -21,6 +29,6 @@ func _physics_process(_delta):
 		array[i].set_collision_mask(2) #collisionMask 2 is the players layer 
 		result = space.intersect_ray(array[i])
 		if result:
-			result.collider.linear_velocity = Vector3(0, 10, 0)
+			result.collider.linear_velocity = rotation.normalized() * fanRange
 			break
 	
