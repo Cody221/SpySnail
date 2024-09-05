c801d3c69ca324fcb025d479cbeafdd866d9b65d
DESKTOP-NPF4KP6\AIMM_Default <clrhodes21@my.trine.edu>
2024-09-05 10:41:42 -0400

    A MINORRR

--- Scripts/BallMovement.gd
+++ Scripts/BallMovement.gd
@@ -6,22 +6,32 @@ var direction = Vector3.ZERO
 var start = Vector3.ZERO
 var end = Vector3.ZERO
 
+
+@export var moveSpeed : int
 @onready var cam = $YGimbal/XGimbal/Camera3D
 
 func _ready():
 	DebugLayer.draw.add_vector(self, "direction", 1, 4, Color(0, 1, 0, 0.5))
+	pass
 
-func _physics_process(_delta):
+func _physics_process(delta):
+	var initPos = Vector3.ZERO
 	if(Input.is_action_just_pressed("LeftMouse")):
-		start = cam.project_position(get_viewport().get_mouse_position(), cam.position.z)
+		initPos = get_viewport().get_mouse_position()
+		initPos = cam.project_position(initPos, cam.position.z)
+		start = position
+		
 	
 	if(Input.is_action_pressed("LeftMouse")):
 		end = get_viewport().get_mouse_position()
 		end = cam.project_position(end, cam.position.z)
+		if((initPos - end).length() <= 0.5):
+			return
 		direction = start - end
 	
 	if(Input.is_action_just_released("LeftMouse")):
-		linear_velocity = direction
+		#need to factor camera position into how the force is applied 
+		linear_velocity = Vector3(direction.x, 0, direction.z) * moveSpeed
 		direction = Vector3.ZERO
 		
 
--- Scripts/PushByFan.gd
+++ Scripts/PushByFan.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var fanRange: int
+@export var fanPower : int
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
@@ -8,8 +9,14 @@ func _physics_process(_delta):
 	
 	#start of ray is position and range is the fan range
 	#seperate query for each point around the fan plus the center 
-	var query0 = PhysicsRayQueryParameters3D.create(position, global_transform.basis.y.normalized() * fanRange)
-	#var query1 = PhysicsRayQueryParameters3D.create(position + , global_transform.basis.y.normalized() * fanRange)
+	
+	#var localXAxis = Vector3(transform.basis.x).normalized() * 1.2
+	#var localYAxis = Vector3(transform.basis.y)
+	#var localZAxis = Vector3(transform.basis.z)
+	#var res = Vector3(global_position.x * 1.1, global_position.y, global_position.z * 1.1)
+	
+	var query0 = PhysicsRayQueryParameters3D.create(global_position, global_transform.basis.y.normalized() * fanRange, 2)
+	#var query1 = PhysicsRayQueryParameters3D.create(res, global_transform.basis.y.normalized() * fanRange, 2)
 	#var query2 = PhysicsRayQueryParameters3D.create(position, global_transform.basis.y.normalized() * fanRange)
 	#var query3 = PhysicsRayQueryParameters3D.create(position, global_transform.basis.y.normalized() * fanRange)
 	#var query4 = PhysicsRayQueryParameters3D.create(position, global_transform.basis.y.normalized() * fanRange)
@@ -22,6 +29,6 @@ func _physics_process(_delta):
 		array[i].set_collision_mask(2) #collisionMask 2 is the players layer 
 		result = space.intersect_ray(array[i])
 		if result:
-			result.collider.linear_velocity = global_transform.basis.y * fanRange
+			result.collider.linear_velocity = global_transform.basis.y * fanPower
 			break
 	
