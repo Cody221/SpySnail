690f0f121b27f3e6527b84aab2275358eabb5319
Cody <rhodescody20@gmail.com>
2024-09-04 23:04:38 -0400

    almost working fan, it factors in the rotation but it's not consistent in pushing

--- Scripts/PushByFan.gd
+++ Scripts/PushByFan.gd
@@ -5,30 +5,23 @@ extends Node3D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	var space = get_world_3d().direct_space_state
+	
 	#start of ray is position and range is the fan range
 	#seperate query for each point around the fan plus the center 
-	
-	#need to factor the rotation of x and z axis to correctly push ball
-	var fanDirection = Vector3(rotation.x, 0, rotation.z).normalized()
-
-	
-	var r = Vector3(fanRange, fanRange, fanRange).bounce(rotation)
-	global_rotation
-	
-	var query0 = PhysicsRayQueryParameters3D.create(position, position + rotation.normalized() * fanRange)
-	#var query1 = PhysicsRayQueryParameters3D.create(position + Vector3(0, 0, 0.5), position + rangeVector)
-	#var query2 = PhysicsRayQueryParameters3D.create(position + Vector3(0, 0, -0.5), position + rangeVector)
-	#var query3 = PhysicsRayQueryParameters3D.create(position + Vector3(0.5, 0, 0), position + rangeVector)
-	#var query4 = PhysicsRayQueryParameters3D.create(position + Vector3(-0.5, 0, 0), position + rangeVector)
+	var query0 = PhysicsRayQueryParameters3D.create(position, global_transform.basis.y.normalized() * fanRange)
+	#var query1 = PhysicsRayQueryParameters3D.create(position + , global_transform.basis.y.normalized() * fanRange)
+	#var query2 = PhysicsRayQueryParameters3D.create(position, global_transform.basis.y.normalized() * fanRange)
+	#var query3 = PhysicsRayQueryParameters3D.create(position, global_transform.basis.y.normalized() * fanRange)
+	#var query4 = PhysicsRayQueryParameters3D.create(position, global_transform.basis.y.normalized() * fanRange)
 	
 	var array = [query0]
 	
 	var result
-	
+	 
 	for i in array.size():
 		array[i].set_collision_mask(2) #collisionMask 2 is the players layer 
 		result = space.intersect_ray(array[i])
 		if result:
-			result.collider.linear_velocity = rotation.normalized() * fanRange
+			result.collider.linear_velocity = global_transform.basis.y * fanRange
 			break
 	
