8bec57355f70154768ae16c8e10070a3952cec74
Cody <rhodescody20@gmail.com>
2024-09-17 15:50:05 -0400

    working teleporter script

--- Scripts/Teleporter.gd
+++ Scripts/Teleporter.gd
@@ -1,13 +1,21 @@
-extends Node3D
+extends Area3D
 
-@onready var start = $StartArea
-@onready var end = $EndArea
-
-
+@export var otherTP : Node3D
+var allowTP = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	connect("body_entered", _on_area_body_entered)
+	connect("body_exited", _on_area_body_exited)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
+func _on_area_body_entered(body):
+	if allowTP:
+		body.position = otherTP.position
+		otherTP.allowTP = false
+		
+func _on_area_body_exited(body):
+	allowTP = true
+
