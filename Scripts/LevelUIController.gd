548c94e9fc48e5475bc6650744f64de2c58c2e43
DESKTOP-NPF4KP6\AIMM_Default <clrhodes21@my.trine.edu>
2024-09-26 17:42:00 -0400

    more work done on tutorial/playtest map, animated a sign to use, setup themeing a little bit for the rest of the UI, should implement themeing on mainmenu. Need to finish playtest map, add springarm to camera, and get more work done on mapmaker. Also edit main menu to support multiple levels when clicking play

--- Scripts/LevelUIController.gd
+++ Scripts/LevelUIController.gd
@@ -1,6 +1,6 @@
 extends MarginContainer
 
-@onready var timeLabel = $HBoxContainer/Label
+@onready var timeLabel = $HBoxContainer/VBoxContainer/Label
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
