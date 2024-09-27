54dab0375faba336d3ab6de567d104a0c8128ccf
DESKTOP-NPF4KP6\AIMM_Default <clrhodes21@my.trine.edu>
2024-09-27 12:58:52 -0400

    stuff done

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,10 +1,11 @@
 extends Node
 
 var stopwatch : Stopwatch
-var summaryWindow = preload("res://Scenes/Menus/SummaryWindow.tscn").instantiate()
+signal level_complete
 
 func _ready():
 	stopwatch = Stopwatch.new()
+
 
 func _process(delta):
 	stopwatch._process(delta)#need to call stopwatch tick function
@@ -17,14 +18,12 @@ func _input(event):
 func ShowEndScreen():
 	#probably want to display an end of game summary
 	stopwatch.isPaused = true
-	add_child(summaryWindow)
-	# X / 3 stars; finished in X time; etc. 
+	level_complete.emit()
+	# X / 3 stars; finished in X time; etc.
 
 func Reset():
 	DebugLayer.draw.vectors = [] #need to reset the vectors to draw so they don't try drawing vectors on the freed objects after reload 
 	stopwatch.currentTime = 0
-	if summaryWindow.is_inside_tree():
-		remove_child(summaryWindow)
 	get_tree().reload_current_scene()
 	stopwatch.isPaused = false
 
--- Scripts/LevelUIController.gd
+++ Scripts/LevelUIController.gd
@@ -1,7 +1,22 @@
 extends MarginContainer
 
 @onready var timeLabel = $HBoxContainer/VBoxContainer/Label
+@onready var endofLevelTime = $SummaryWindow/HBoxContainer/VBoxContainer/Label
+@onready var restartButton = $SummaryWindow/HBoxContainer/VBoxContainer/RestartButton
+@onready var returnButton = $SummaryWindow/HBoxContainer/VBoxContainer/ReturnButton
+@onready var sumWindow = $SummaryWindow
+
+func _ready():
+	restartButton.pressed.connect(GameManager.Reset)
+	returnButton.pressed.connect(GameManager.EndLevel)
+	GameManager.level_complete.connect(sumWindow.show)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	timeLabel.text = "Time:  " + str(snapped(GameManager.stopwatch.currentTime, 0.01))
+	endofLevelTime.text = "Time: " + str(snapped(GameManager.stopwatch.currentTime, 0.01))
+
+
+
+
